fd885210070b74d1d3767bb8c8d65444c9d8657b
pizzo1jr <josephpizzo@rocketmortgage.com>
2023-04-14 09:09:52 -0400

    js updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,20 @@
-preventDefault();
+globalThis.myApp = {
+    copy: (propertyHubForm) => {
+        var propertyHubForm = document.getElementById("#property-hub-form");
+        let copyText = [];
+        for (const key in propertyHubForm) {
+            if (Object.hasOWnProperty.call(propertyHubForm, key)) {
+                const val = propertyHubForm[key].value;
+                if (val) {
+                    copyText.push(propertyHubForm [key].value);
+                }
+            }
+        }
+        navigator.clipboard
+            .writeText(copyText.join('\n'))
+            .then(() => console.log("form values copied"))
+            .catch((err) => console.log(err));
+    }
+};
+
+button.addEventListener("click". globalThis.myApp.copy);
