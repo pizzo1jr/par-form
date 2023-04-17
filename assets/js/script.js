5096c4f88c3f22dcd71eb459a95417ee79b549e5
pizzo1jr <josephpizzo@rocketmortgage.com>
2023-04-17 13:33:54 -0400

    copy works

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,48 @@
-globalThis.myApp = {
-    copy: (form) => {
-        var form = document.getElementById('#property-hub-form');
-        let copyText = [];
-        for (const key in form) {
-            if (Object.hasOWnProperty.call(form, key)) {
-                const val = form[key].value;
-                if (val) {
-                    copyText.push(form [key].value);
-                }
-            }
-        }
-        navigator.clipboard
-            .writeText(copyText.join('\n'))
-            .then(() => console.log("form values copied"))
-            .catch((err) => console.log(err));
-    }
-};
+const allowedCopyElements = ["INPUT", "TEXTAREA"];
 
-// object.addEventListener("click". globalThis.myApp);
+async function copy(formElementId) {
+  var form = document.getElementById(formElementId);
+  await copyToClipboard([form]);
+}
+
+async function copyAll() {
+  var forms = document.getElementsByTagName("form");
+  await copyToClipboard([...forms]);
+}
+
+async function copyToClipboard(forms) {
+  let formStringArray = [];
+  for (let index = 0; index < forms.length; index++) {
+    const result = convertFormToTextArray(forms[index]); // Ex: ['Property Hub', 'Seller Mismatch: ob9qb', 'Property Type: 2cma6h', 'Last Date of Sale: 2b43y', 'Additional Notes: s3q2j']
+    if (result.length === 0) continue;
+
+    if (index < forms.length - 1)
+      result[result.length - 1] = `${result[result.length - 1]}\n`;
+    const resultString = result.join("\n"); // Ex: Property Hub\nSeller Mismatch: ob9qb\nProperty Type: 2cma6h\nLast Date of Sale: 2b43y\nAdditional Notes: s3q2j\n
+    formStringArray.push(resultString);
+  }
+
+  await writeToClipboard(formStringArray.join("\n"));
+}
+
+async function writeToClipboard(data) {
+  try {
+    await navigator.clipboard.writeText(data);
+  } catch (error) {
+    console.error("Could not write to clipboard", error);
+  }
+}
+
+function convertFormToTextArray(form) {
+  const output = [];
+  const formElements = form.elements;
+  for (let index = 0; index < formElements.length; index++) {
+    const element = formElements[index];
+    if (element.value && allowedCopyElements.includes(element.nodeName)) {
+      const label = element.labels[0].textContent;
+      output.push(`${label} ${element.value}`);
+    }
+  }
+  if (output.length > 0) output.unshift(`${form.name}`);
+  return output;
+}
